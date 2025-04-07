76f9d0faa08b8b65a055b0dea22d6e889691a243
Ronan Jahier <ronan.jahier@orange.fr>
2025-04-07 17:13:49 +0200

    modifs légères

--- chatbot_gemini_bro.py
+++ chatbot_gemini_bro.py
@@ -7,34 +7,35 @@ st.title("🦜 Gemini For My Bro'")
 
 if "api_key" not in st.session_state:
     st.session_state.api_key = st.secrets["GEMINI_API_KEY"]
- 
+
+# ce compteur permet de rajouter le "contexte système" régulièrement dans la mémoire du bot 
 if "compteur" not in st.session_state:
     st.session_state.compteur = 0
     
 if "contexte_systeme" not in st.session_state:
-    st.session_state.contexte_systeme = """N'oublie pas, tu parles de façon décontractée avec un peu d'humour voire une pointe d'ironie, mais tu reste toujourstrès rigoureux dans la quelité des tes réponses. Dès que tu peux apporter une anedocte sur la danse ou la physique, en particulier la physique quantique, tu t'empresses de le faire. Tu ajoutes souvent des citations de physiciens à la fin de ta réponse."""
+    st.session_state.contexte_systeme = """N'oublie pas, tu parles de façon décontractée avec un peu d'humour voire une pointe d'ironie, mais tu reste toujours très rigoureux dans la qualité des tes réponses. Dès que tu peux apporter une anedocte sur la danse ou la physique, en particulier la physique quantique, tu t'empresses de le faire. Tu ajoutes souvent des citations de physiciens à la fin de ta réponse."""
 
-# Initialize chat history
+# Initialisation de l'historique du chat
 if "messages" not in st.session_state: 
     st.session_state.messages = [SystemMessage(st.session_state.contexte_systeme)]
 
-def generate_response(): 
+def generer_response(): 
     chat_model = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.7, api_key=st.session_state.api_key)
     reponse = chat_model.invoke(st.session_state.messages).content
-    st.session_state.messages = st.session_state.messages + [AIMessage(reponse)]
+    st.session_state.messages.append(AIMessage(reponse))
     st.info(reponse)
 
 with st.form("chat_form"):
     question = st.text_area(
         "Écris ta question","..."
     )
-    submitted = st.form_submit_button("Soumettre") 
-    if submitted:
+    form_soumis = st.form_submit_button("Soumettre") 
+    if form_soumis:
         st.session_state.messages.append(HumanMessage(question))
-        st.session_state.messages = st.session_state.messages[-40:]
-        generate_response()
+        st.session_state.messages = st.session_state.messages[-40:] # on ne conserve que 40 messages en mémoire
+        generer_response()
         st.session_state.compteur +=  1
-    if st.session_state.compteur % 40 ==  0:
+    if st.session_state.compteur % 40 ==  0: # rafraîchissement du "contexte système"
         st.session_state.messages.append(SystemMessage(st.session_state.contexte_systeme))
 
 st.header('Historique')
